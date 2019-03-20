4d12d0893f712af57f4728b337f80d33d155f3e0
Jacob Alber <jaalber@microsoft.com>
2019-03-20 14:41:06 -0400

    Create Azure Pipeline for Windows Build

--- .scripts/init.cmd
+++ .scripts/init.cmd
@@ -7,7 +7,23 @@ IF NOT DEFINED msbuildPath (
     SET msbuildPath=msbuild
 )
 
+IF NOT DEFINED vstestPath {
+    SET vstestPath=vstest.console
+}
+
 REM Repo-specific paths
 IF NOT DEFINED vwRoot (
     SET vwRoot=%~dp0..
+)
+
+IF NOT DEFINED Version (
+    SET Version=8.6.1
+)
+
+IF NOT DEFINED VowpalWabbitAssemblyVersion (
+    SET VowpalWabbitAssemblyVersion=%Version%
+)
+
+IF NOT DEFINED Tag (
+    SET Tag=-INTERNALONLY
 )
