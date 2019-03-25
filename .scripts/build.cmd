e113aae1e6db25462bb3bd923f3741dcb1a0d719
Jacob Alber <jalber@fernir.com>
2019-03-25 12:49:23 -0400

    wip commit to debug build issues in vw_core.vcxproj under VS2017+

--- .scripts/build.cmd
+++ .scripts/build.cmd
@@ -9,8 +9,17 @@ CALL %~dp0init.cmd
 PUSHD %~dp0
 
 REM TODO: Figure out how to parametrize this script?! (is there a standard, or do we actually need parse args?)
-ECHO Building "%vwRoot%\vowpalwabbit\vw.sln" for Release x64
-"%msbuildPath%" "%vwRoot%\vowpalwabbit\vw.sln" /nr:false /v:normal /m /p:Configuration=Release;Platform=x64 
+
+REM ECHO Running make_config to propagate version
+REM PUSHD "%vwRoot%\vowpalwabbit"
+
+REM REM This is idempotent, and for some reason is not triggering from the build...
+REM "win32\make_config_h.exe"
+
+REM POPD
+
+ECHO Building "%vwRoot%\vowpalwabbit\vw_fix.sln" for Release x64
+"%msbuildPath%" "%vwRoot%\vowpalwabbit\vw_fix.sln" /nr:false /v:normal /m /p:Configuration=Release;Platform=x64
 
 POPD
 
