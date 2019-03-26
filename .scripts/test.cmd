22c566658714f3a670f755f155487a22fda1f4e4
Jacob Alber <jalber@fernir.com>
2019-03-26 14:31:48 -0400

    Fix typo in test.cmd (wrong envvar for error code)

--- .scripts/test.cmd
+++ .scripts/test.cmd
@@ -11,26 +11,27 @@ REM TODO: Ensure all errors caught by the smoke test will result in non-zero exi
 ECHO Running VW C Smoke test
 "%vwRoot%\vowpalwabbit\out\target\Release\x64\c_test.exe"
 
-IF "%EXITCODE%" NEQ "0" (
+IF "%ERRORLEVEL%" NEQ "0" (
+    ECHO WTF?!
     ENDLOCAL
-    EXIT /B %EXITCODE%
+    EXIT /B %ERRORLEVEL%
 )
 
 ECHO Running VW Unit Tests in C++
 "%vwRoot%\vowpalwabbit\out\target\Release\x64\vw_unit_test.exe"
 
-IF "%EXITCODE%" NEQ "0" (
+IF "%ERRORLEVEL%" NEQ "0" (
     ENDLOCAL
-    EXIT /B %EXITCODE%
+    EXIT /B %ERRORLEVEL%
 )
 
 ECHO Running VW Unit Tests in C#
 REM TODO: Add explicit logging configuration so it can be uploaded to pipeline results.
 %vstestPath% /Platform:x64 /inIsolation "%vwRoot%\vowpalwabbit\x64\Release\cs_unittest.dll" /TestCaseFilter:"TestCategory!=NotOnVSO"
 
-IF "%EXITCODE%" NEQ "0" (
+IF "%ERRORLEVEL%" NEQ "0" (
     ENDLOCAL
-    EXIT /B %EXITCODE%
+    EXIT /B %ERRORLEVEL%
 )
 
 REM this is the same as above, just xcopied to the out folder. This is broken due to baked-in paths. The fix is to place the output straight into out.
