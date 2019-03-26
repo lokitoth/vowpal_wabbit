76db9d332441178114a440ba40288bc4e2db4649
Jacob Alber <jalber@fernir.com>
2019-03-26 16:45:53 -0400

    Fix OutputDir missing in package step

--- .scripts/package.cmd
+++ .scripts/package.cmd
@@ -15,7 +15,9 @@ SET SolutionDir=%vwRoot%\vowpalwabbit\
 
 REM Do not add a trailing backslash here, as it will break the nuget xml parser for some reason
 SET RepoRoot=%vwRoot%
-SET OutputDir="%SolutionDir%out\target\x64\Release"
+SET OutputDir="%SolutionDir%out\package\Release\x64"
+
+IF NOT EXIST %OutputDir% MKDIR %OutputDir%
 
 REM TODO-pre-checkin: Figure out how to parametrize this script?! (is there a standard, or do we actually need parse args?)
 "%nugetPath%" pack %SolutionDir%..\cs\cs\cs.nuspec -OutputDirectory %OutputDir% -Verbosity detailed -BasePath "." -Properties "RootRelativeOutputDirX64=%RootRelativeOutputDirX64%;RootRelativeOutputDirAnyCPU=%RootRelativeOutputDirAnyCPU%;Configuration=Release;Platform=X64;version=%Version%;Tag=%Tag%;RepoRoot=%RepoRoot%;SolutionDir=%SolutionDir%"
